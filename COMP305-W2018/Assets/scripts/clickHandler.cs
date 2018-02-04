ffc7f9ad2ecb67efd383c357171557a97ac3177e
Devdas2018 <dchatte2@my.centennialcollege.ca>
2018-02-04 16:40:37 -0500

    Lab02 scene created

---
+++ COMP305-W2018/Assets/scripts/clickHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickHandler : MonoBehaviour {
+    //private int someNumber;
+    public GameObject PixelguyObject;
+    public Sprite sprite;
+    private SpriteRenderer spriteRenderer;
+    Rigidbody2D rBody;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Debug.Log(someNumber);
+        //rBody= PixelguyObject.GetComponent<Rigidbody2D>();
+        spriteRenderer = PixelguyObject.GetComponent<SpriteRenderer>();
+        //rBody.velocity = new Vector2(2, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnMouseDown()
+    {
+        //rBody.velocity*=-1;
+        spriteRenderer.sprite = sprite;
+       
+    }
+    
+}
---
+++ COMP305-W2018/Assets/scripts/mover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mover : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Rigidbody2D rBody=this.gameObject.GetComponent<Rigidbody2D>();
+        rBody.velocity = new Vector2(2, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void FixedUpdate()
+    {
+
+    }
+}
